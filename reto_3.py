25f7c7edc06e790a43f8c95c396479001d518dce
pablo salazar <75328698+pablo19-alexander@users.noreply.github.com>
2022-05-25 21:24:48 -0500

    Update reto_3.py

--- reto_3.py
+++ reto_3.py
@@ -1,14 +1,22 @@
 def AutoPartes(ventas: list):
-    salida = dict()
-    for venta in ventas:
-        salida[venta[0]] =
+    # Inicializar diccionario
+    venta = dict()
+    # Ciclo para agregar un nuevo evento
+    for idProducto, dProducto, pnProducto, cvProducto, sProducto, nComprador, cComprador, fVenta in ventas:
+        # Fuerza la entrada
+        if venta.get(idProducto) is None:
+            # Creacion de un nuevo evento
+            venta[idProducto] = []
+            # Registro de una nueva lista de tuplas en el dict agenda
+        venta[idProducto].append((dProducto, pnProducto, cvProducto, sProducto, nComprador, cComprador, fVenta))
+    return venta
 
 def consultaRegistro(ventas, idProducto):
-    pass
-
-consultaRegistro(AutoPartes([
-    (5489,'tornillo', 'RS8512',2,33,'Julio Perez',3654213,'13/06/2020'),
-    (3215,'zocalo', 'UM8587',2,125,'Laura Macias',1256321,'13/06/2020'),
-    (3698,'biela', 'PT3218',1,78,'Luis Peña',14565487,'13/06/2020'),
-    (2001,'cilindro', 'AZ8794',2,96,'Carlos Casio',5612405,'13/06/2020')]), 2001)
-print()
+    if idProducto in ventas.keys():
+        for dProducto, pnProducto, cvProducto, sProducto, nComprador, cComprador, fVenta in ventas[idProducto]:
+            print(
+                f"Producto consultado : {idProducto}  Descripción  {dProducto}  #Parte  {pnProducto}  Cantidad vendida  {cvProducto}  Stock  {sProducto}  Comprador {nComprador}  Documento  {cComprador}  Fecha Venta  {fVenta}"
+            )
+            
+    else:
+        print("No hay registro de venta de ese producto")
